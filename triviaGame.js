5bc501db95cdc8298c674b74ecbbaaca4a4956dd
SensAsian <54989736+SensAsian@users.noreply.github.com>
2020-11-01 08:58:22 -0500

    Just a few fun functions

--- triviaGame.js
+++ triviaGame.js
@@ -1,4 +1,7 @@
-  function keepScore () {
-    var playing = true;
-    
-  }
+function change() { //allows you to click on text and change it to this "It's November!"
+  document.getElementById("center").innerHTML = "It's November!";
+}
+
+function buttonHit (){ //let's you click the button
+  var press = document.getElementById("TRUE");
+}
